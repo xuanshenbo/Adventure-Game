6b0f50f2cf51d267d212da31946d00e5fa82251f
o0o0ol <lucas@LucastekiMacBook-Pro.local>
2015-09-19 21:59:43 +1200

    basic constructor for renderer

--- renderer/Renderer.java
+++ renderer/Renderer.java
@@ -1,16 +1,39 @@
 package renderer;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
 import state.Area;
 
-public class Renderer {
+public class Renderer extends JPanel{
+	
+	private int width,height;
+	private Image boardImg;
+	
+	public Renderer(int width, int height){
+		
+		this.width = width;
+		this.height = height;
+		Dimension size = new Dimension(width, height);
 
-	public void drawArea(Area area) {
-
+//		boardImg = new ImageIcon("Images/Board.jpg").getImage();
+		
+		setPreferredSize(size);
+		setBorder(BorderFactory.createLineBorder(Color.black));
+		setVisible(true);
 	}
-
-	public void repaint() {
-		// TODO Auto-generated method stub
-
+	
+	public void paintComponent(Graphics g) {
+		//clear this panel
+		this.removeAll();
+		//draw the game board
+			g.drawImage(boardImg, 0, 0, width, height, null);
 	}
 
 }
